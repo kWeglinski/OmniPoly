3dc5341f50f1ce3aae71d93a9e37dc2ba0874dec
Krzysztof Węgliński <krzysiek@sth.io>
2025-07-14 13:06:24 +0800

    Add unit tests for translate utils and API functions (#21)

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text'],
+};
